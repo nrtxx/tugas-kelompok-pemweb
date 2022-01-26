e16a12c060253ac12be7c64adebe84848144c586
nrtxx <rangganurtakusumah@gmail.com>
2022-01-26 23:25:54 +0700

    halaman tips

---
+++ codes/tips.php
@@ -0,0 +1,46 @@
+<?php
+    include 'header.php';
+?>
+
+<main>
+        <div class="container mt-5 mb-3">
+            <h1>Smart Tips</h1>
+            <img src="assets/img/kebun.jpeg" alt="Kebun" class="col-md-6 offset-md-3 my-3 d-flex justify-content-start">
+            <p>Untuk mengisi waktu luang, kamu bisa mencoba bercocok tanam. Beberapa pilihan
+                tanaman bisa kamu coba, salah satunya menanam sayuran. Berkebun sayur adalah salah satu cara untuk
+                menghijaukan rumah sekaligus menghemat uang. Karena, menanam sendiri sayuranmu, artinya kamu tak perlu
+                berbelanja dan selalu mendapatkan bahan makanan yang sehat dan segar. Jika kamu ingin mengawali menanam
+                sayuran, yang dibutuhkan adalah kesabaran dan taktik yang cerdas untuk mendapatkan hasil yang maksimal
+                dari kebun sayurmu.</p>
+            <p>Untuk kamu yang baru saja memulai, berikut tips berkebun sayuran:</p>
+            <p class="fw-bolder">1. Sinar Matahari</p>
+            <p>Sinar matahari langsung penting agar sayuran bisa tumbuh dengan baik. Area yang dipilih untuk tanaman
+                harus terkena sinar matahari minimal 6-8 jam sehari. Meski rata-rata tanaman sayur membutuhkan matahari,
+                ada juga sayuran yang tidak terlalu membutuhkannya. Cari tahu jenis sayuran yang kamu tanam dan
+                kebutuhannya akan sinar matahari.</p>
+            <p class="fw-bolder">2. Air</p>
+            <p>Pastikan area yang kamu pilih memiliki sumber air yang baik karena sayuran membutuhkan air secara teratur
+                untuk tumbuh. Juga pastikan area tersebut bebas hama.</p>
+            <p class="fw-bolder">3. Siapkan Media Tanam</p>
+            <p>Tanah adalah dalah satu faktor terpenting yang harus dipertimbangkan saat menanam kebun sayur. Untuk
+                media tanam, atau dalam hal ini tanah harus kaya bahan organik. Tanah yang kaya bahan organik memiliki
+                semua jenis organisme yang bermanfaat bagi tanaman.</p>
+            <p class="fw-bolder">4. Pupuk Organik</p>
+            <p>Cobalah membeli produk berkebun organik daripada pupuk sintetis dan bahan kimia. Beri juga tanaman air,
+                matahari, dan nutrisi adalah cara tercepat untuk menangkal hama dan penyakit yang membuatmu sehat.</p>
+            <p class="fw-bolder">5. Peralatan Berkebun</p>
+            <p>Alat yang tepat dibutuhkan untuk menyiapkan dan merawat tanaman. Kamu membutuhkan sekop, garpu sekop,
+                untuk membuat taman. Kamu harus membeli selang dan nozel atau sistem irigasi setelah tanaman mulai
+                tumbuh.</p>
+            <p class="fw-bolder">6. Siapkan Bedengan</p>
+            <p>Jika kamu akan menanamnya di depan rumah, buatlah bedengan untuk membulatkan tanah. Bedengan yang
+                ditinggikan menumbuhkan tanah lebih tinggi dari tanah setinggi 15cm atau lebih.</p>
+            <p class="fw-bolder">7. Atur Tanaman</p>
+            <p>Perhatikan bagaimana kamu mengatur tanaman. Maksimalkan ruang namun juga perhatikan jarak agar tanaman
+                tetap bisa leluasa tumbuh.</p>
+        </div>
+    </main>
+
+<?php
+    include 'footer.php';
+?>
